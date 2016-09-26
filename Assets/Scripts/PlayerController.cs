3e7a59feb408ccc2c31b5e8612bd6a36e5a06295
Jonathan Morales <jonmorales22@gmail.com>
2016-09-25 22:15:06 -0400

    Changed player movement based on textbook. animations no longer work, gonna implement later. Currently working on jumping.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,30 +2,35 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
+	[HideInInspector]
+	public bool isFacingRight = true;
+	[HideInInspector]
+	public bool isJumping = false;
+	[HideInInspector]
+	public bool isGrounded = false;
 
-	public SpriteRenderer c;
-	public float walkSpeed = 1.0f;
-	public float runSpeed=3.0f;
+	public float maxSpeed=7.0f;
+	public float jumpForce = 750.0f; 
+
+
+	public Transform groundCheck;
+	public LayerMask groundLayers;
+
 	public Animator anim;
 	public AudioClip footstep;
 	public AudioClip deathsound;
 
+
+	private float groundCheckRadius = .02f;
 	private AudioSource audiosource;
-	private bool isWalking = false;
 	private bool isDead = false;
-	private int direction=(int)facing.RIGHT;
-	private enum facing {RIGHT,LEFT};
-	private float speed;
-	private Vector3 movement = new Vector3 (0, 0, 0);
-
-
+	private Rigidbody2D player;
 	// Use this for initialization
 	void Awake(){
 		audiosource = this.GetComponent<AudioSource>();
 		anim = GetComponent<Animator> ();
 		SetAnimationController (0);
-		speed = walkSpeed;
-		//
+		player = GetComponent<Rigidbody2D> ();
 
 	}
 
@@ -34,63 +39,42 @@ public class PlayerController : MonoBehaviour {
 		audiosource.clip = footstep;
 		audiosource.Play ();
 	}
+	void Update(){
+		if (Input.GetButtonDown ("Jump")) {
+			if (isGrounded) {
+				player.velocity = new Vector2 (player.velocity.x, 0);
+				player.AddForce (new Vector2 (0, jumpForce));
+			}
+		}
 	
+	}
 	// Update is called once per frame
-	void LateUpdate () {
+	void FixedUpdate () {
 		if (!isDead) {
-			if (Input.GetKeyUp ("right")) {
-				if (isWalking && direction == (int)facing.RIGHT) {
-					speed = runSpeed;
-					anim.SetInteger ("AnimationState", 2);
-				} else if (!isWalking) {
-					speed = walkSpeed;
-					isWalking = true;
-					anim.SetInteger ("AnimationState", 1);
-				}
+			
+			isGrounded=Physics2D.OverlapCircle(groundCheck.position,groundCheckRadius,groundLayers);
+			float move = Input.GetAxis ("Horizontal");
+			player.velocity = new Vector2 (move * maxSpeed, player.velocity.y);
 
-				c.flipX = false;
-				direction = (int)facing.RIGHT;
-				movement = new Vector3 (1, 0, 0);
-
-			}
-			if (Input.GetKeyUp ("left")) {
-				if (isWalking && direction == (int)facing.LEFT) {
-					speed = runSpeed;
-					anim.SetInteger ("AnimationState", 2);
-				} else if (!isWalking) {
-					speed = walkSpeed;
-					isWalking = true;
-					anim.SetInteger ("AnimationState", 1);
-				}
-				c.flipX = true;
-				direction = (int)facing.LEFT;
-				movement = new Vector3 (-1, 0, 0);
+			if((move>0.0f&&!isFacingRight) || (move<0.0&& isFacingRight)) {
+				Flip();
 			}
 
-			if (Input.GetKeyUp ("space")) {
-				if (speed == runSpeed) {
-					speed = walkSpeed;
-					//isWalking = true;
-					anim.SetInteger ("AnimationState", 1);
-				} else if (speed == walkSpeed) {
-					Debug.Log("Ass");
-					movement = new Vector3 (0, 0, 0);
-					isWalking = false;
-					anim.SetInteger ("AnimationState", 0);
-				}
-			}
 			if (Input.GetKeyUp (KeyCode.Backspace)) {
-				movement = new Vector3 (0, 0, 0);
-				isWalking = false;
 				anim.SetBool ("isDead", true);
 				isDead = true;
 				playDeathSound ();
 			}
-			//Debug.Log (speed);
-			transform.position += movement * Time.deltaTime * speed;
 		}
 	}
+	void Flip()
+	{
+		isFacingRight = !isFacingRight;
+		Vector3 playerScale = transform.localScale;
+		playerScale.x = playerScale.x*-1;
+		transform.localScale = playerScale;
 
+	}
 	void playDeathSound()
 	{
 		AudioSource.PlayClipAtPoint (this.deathsound,this.transform.position);
