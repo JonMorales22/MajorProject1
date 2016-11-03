6e49988e9d495877fcd6bc6b2578e43fb9cd323b
Jonathan Morales <jonmorales22@gmail.com>
2016-11-03 03:09:00 -0400

    Added rough versions of the saw animations and movement, still kind of broken and needs to be fixed

---
+++ Assets/Scripts/SawScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SawScript : MonoBehaviour {
+	// Use this for initialization
+	public float speed;
+	public Transform[] waypoints;
+	// Update is called once per frame
+	void Start()
+	{
+
+	}
+	void Update () {
+		float delta = speed * Time.deltaTime; 
+		//Debug.Log (Mathf.Abs (transform.position.y) - waypoints [0].position.y);
+		if (transform.position.y - Mathf.Abs(waypoints [0].position.y)<.1f) 
+		{
+			//Debug.Log ("1");
+			Vector2 asdf = new Vector2 (waypoints [1].position.x,waypoints[1].position.y);
+			transform.position = Vector2.MoveTowards (transform.position, asdf, delta);
+		}
+		else 
+		{
+			//Debug.Log ("2");
+			Vector2 asdf = new Vector2 (waypoints [0].position.x,waypoints[0].position.y);
+			transform.position = Vector2.MoveTowards (transform.position, asdf, delta);
+		}
+	}
+}
