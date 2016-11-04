a1dac50ea09b5e988a93158a83c509db89e274e7
Jonathan Morales <jonmorales22@gmail.com>
2016-11-04 01:47:54 -0400

    Started Designing level 1. Also added a script to move platforms based on waypoints.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class CameraController : MonoBehaviour {
 
 	public GameObject player;
+	public float limit;
 
 	private Vector3 offset;
 	// Use this for initialization
@@ -13,7 +14,11 @@ public class CameraController : MonoBehaviour {
 
 	// Update is called once per frame
 	void LateUpdate () {
-		//Vector3 ddd = new Vector3 (player.transform.position.x, 0, 0);
-		transform.position = new Vector3(player.transform.position.x,0,0)+offset;
+		if(player.transform.position.y<limit){
+			//USE THIS IF YOU ONLY WANT CAMERA TO MOVE IN X DIRECTION!!!
+			transform.position = new Vector3(player.transform.position.x+offset.x,0,offset.z);
+		}
+		else if(player.transform.position.y>=limit)
+			transform.position = new Vector3(player.transform.position.x,player.transform.position.y,0)+offset;
 	}
 }
---
+++ Assets/Scripts/MovePlatform.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePlatform : MonoBehaviour {
+	public float speed;
+	public Transform[] waypoints;
+	public bool isMoving;
+
+	private Vector2 asdf;
+	private int index;
+	// Use this for initialization
+	void Start () {
+		index = 0;
+		asdf = new Vector2(waypoints[0].position.x,waypoints[0].position.y);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isMoving) {
+			float delta = speed * Time.deltaTime;
+			transform.position = Vector2.MoveTowards (transform.position, asdf, delta);
+			float distance = Mathf.Sqrt ((transform.position.y - waypoints [index].position.y) * (transform.position.y - waypoints [index].position.y));
+			//Debug.Log (distance);
+			if (distance < .01f) {
+				//Debug.Log ("if");
+				//asdf = new Vector2 (waypoints [1].position.x, waypoints [1].position.y);
+				asdf = GetWayPoint ();
+			}
+		}
+	}
+
+	Vector2 GetWayPoint()
+	{
+		index++;
+		if (index == waypoints.Length)
+			index = 0;
+
+		Vector2 temp = new Vector2 (waypoints[index].position.x, waypoints[index].position.y);
+		return temp;
+	}
+
+	void OnCollisionEnter2D(Collision2D c)
+	{
+		if (c.gameObject.CompareTag ("Player"))
+		{	if(!isMoving)
+				isMoving = true;
+			c.gameObject.transform.SetParent (gameObject.transform);
+		}
+
+
+	}
+
+	void OnCollisionExit2D(Collision2D c)
+	{
+		if (c.gameObject.CompareTag ("Player"))
+		{
+			c.gameObject.transform.SetParent (null);
+		}
+	}
+}
