5276b6e78a93a8f5eab5034e80a6d82a72712d3a
Jonathan Morales <jonmorales22@gmail.com>
2016-11-05 21:39:46 -0400

    Got the Snowball working, it's about 70% there. Just need to adjust the script so that the snowball's scale increases proportionally to the rigidbody's velocity.

---
+++ Assets/Scripts/SnowBallScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SnowBallScript : MonoBehaviour {
+	public float delta;
+	public float deltaMass;
+
+	private Rigidbody2D rb;
+	private Vector3 deltaVector;
+	private bool isGrowing;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		deltaVector = new Vector3 (delta, delta, 0);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Mathf.Abs(rb.velocity.x) > .1)
+		{
+			if (transform.localScale.x < 5&&!isGrowing)
+			{
+				isGrowing = true;
+				StartCoroutine ("Grow");
+			}
+		}
+	}
+
+	IEnumerator Grow()
+	{
+		while (transform.localScale.x < 5&&Mathf.Abs(rb.velocity.x)>.1)
+		{
+			rb.mass += deltaMass;
+			transform.localScale += deltaVector;
+			yield return new WaitForSeconds (.1f);
+		}
+		isGrowing = false;
+	}
+}
