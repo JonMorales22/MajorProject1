2b2e87f8c92ab8962937c5855a8294a78690f0f9
Jonathan Morales <jonmorales22@gmail.com>
2016-11-07 23:14:17 -0500

    Fixed a bug where User was not prompted for name on high score screen after beating the game

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -13,11 +13,14 @@ public class FinishLine : MonoBehaviour {
 			Scene activeScene = SceneManager.GetActiveScene ();
 			//int temp = activeScene.buildIndex;
 			//int numOfScenes = SceneManager.sceneCountInBuildSettings;
-			if (activeScene.buildIndex < SceneManager.sceneCountInBuildSettings-1) {
+			if (activeScene.buildIndex < SceneManager.sceneCountInBuildSettings - 1) {
 				SceneManager.LoadScene (activeScene.buildIndex + 1);
 			}
 			else
+			{
+				PlayerPrefs.SetInt ("247127CurrentPlayerScore", stats.score);
 				SceneManager.LoadScene ("HighScore");
+			}
 		}
 	}
 }
